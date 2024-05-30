ce035d3bdbe24df09ea9a9679dd6110a673f05d4
ErikAndreasKlokk <eaklokk@gmail.com>
2024-05-30 11:15:19 +0200

    andre eksamens commit
    lagt til artikkel side

--- nettside/src/lib/components/header.svelte
+++ nettside/src/lib/components/header.svelte
@@ -2,6 +2,6 @@
 
 </script>
 
-<header>
-    
+<header class=" h-20 border-b border-gray-400 flex justify-center items-center p-2 mb-10">
+    <a href="/" class=" font-bold text-2xl underline ">Hjem</a>
 </header>
---
+++ nettside/src/routes/blogg/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import {artikkel} from '$lib/stores/artikkel'
+</script>
+
+<main class=" w-screen flex justify-center">
+    <div class=" px-4 w-fit place-items-center lg:max-w-[1200px]">
+        <p>{$artikkel.Tittel}</p>
+    </div>
+</main>
